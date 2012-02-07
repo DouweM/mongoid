fa547ce031829e2342ad2e64f4fdbc58f606d7f8
Douwe Maan <douwemaan@me.com>
2012-02-07 22:02:56 +0100

    Get stuff working on 2.4.0-stable.

--- spec/functional/mongoid/relations/accessors_spec.rb
+++ spec/functional/mongoid/relations/accessors_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Mongoid::Relations::Accessors do
 
   before do
-    [ Book, Movie, Rating, Person, Preference, Game ].each(&:delete_all)
+    [ Book, Movie, Rating, Person, Preference, Game, Face, Eye, EyeBowl ].each(&:delete_all)
   end
 
   describe "\#{getter}" do
